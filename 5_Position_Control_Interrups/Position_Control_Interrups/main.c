4e3ee7d8012206de72ac7ae9a3c12c6393c66b8a
Tejas kachare <55286055+tejas-kach@users.noreply.github.com>
2020-03-23 18:05:55 +0530

    Add files via upload

---
+++ 5_Position_Control_Interrups/Position_Control_Interrups/main.c
@@ -0,0 +1,574 @@
+/********************************************************************************
+ Written by: Vinod Desai, NEX Robotics Pvt. Ltd.
+ Edited by: Sachitanand Malewar, NEX Robotics Pvt. Ltd.
+ AVR Studio Version 4.17, Build 666
+
+ Date: 26th December 2010
+
+ This experiment demonstrates use of position encoders.
+
+ Concepts covered: External Interrupts, Position control
+ 
+ Microcontroller pins used:
+ PORTA3 to PORTA0: Robot direction control
+ PL3, PL4: Robot velocity control. Currently set to 1 as PWM is not used
+ PE4 (INT4): External interrupt for left motor position encoder 
+ PE5 (INT5): External interrupt for the right position encoder
+
+ Note: 
+ 
+ 1. Make sure that in the configuration options following settings are 
+ 	done for proper operation of the code
+
+ 	Microcontroller: atmega2560
+    Frequency: 14745600
+ 	Optimization: -O0  (For more information read section: Selecting proper optimization 
+ 					options below figure 2.22 in the Software Manual)
+
+ 2.	It is observed that external interrupts does not work with the optimization level -Os
+
+ 3. Auxiliary power can supply current up to 1 Ampere while Battery can supply current up to 
+ 	2 Ampere. When both motors of the robot changes direction suddenly without stopping, 
+	it produces large current surge. When robot is powered by Auxiliary power which can supply
+	only 1 Ampere of current, sudden direction change in both the motors will cause current 
+	surge which can reset the microcontroller because of sudden fall in voltage. 
+	It is a good practice to stop the motors for at least 0.5seconds before changing 
+	the direction. This will also increase the useable time of the fully charged battery.
+	the life of the motor.
+
+*********************************************************************************/
+
+/********************************************************************************
+
+   Copyright (c) 2010, NEX Robotics Pvt. Ltd.                       -*- c -*-
+   All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are met:
+
+   * Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+
+   * Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in
+     the documentation and/or other materials provided with the
+     distribution.
+
+   * Neither the name of the copyright holders nor the names of
+     contributors may be used to endorse or promote products derived
+     from this software without specific prior written permission.
+
+   * Source code can be used for academic purpose. 
+	 For commercial use permission form the author needs to be taken.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+  POSSIBILITY OF SUCH DAMAGE. 
+
+  Software released under Creative Commence cc by-nc-sa licence.
+  For legal information refer to: 
+  http://creativecommons.org/licenses/by-nc-sa/3.0/legalcode
+
+********************************************************************************/
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+unsigned long int ShaftCountLeft = 0; //to keep track of left position encoder 
+unsigned long int ShaftCountRight = 0; //to keep track of right position encoder
+unsigned int Degrees; //to accept angle in degrees for turning
+unsigned int temp;
+
+unsigned char ADC_Conversion(unsigned char);
+unsigned char ADC_Value;
+unsigned char flag = 0;
+unsigned char Left_white_line = 0;
+unsigned char Center_white_line = 0;
+unsigned char Right_white_line = 0;
+unsigned char THRESHOLD=42;
+
+//Function to configure ports to enable robot's motion
+void motion_pin_config (void) 
+{
+ DDRA = DDRA | 0x0F;
+ PORTA = PORTA & 0xF0;
+ DDRL = DDRL | 0x18;   //Setting PL3 and PL4 pins as output for PWM generation
+ PORTL = PORTL | 0x18; //PL3 and PL4 pins are for velocity control using PWM.
+}
+
+//Function to configure INT4 (PORTE 4) pin as input for the left position encoder
+void left_encoder_pin_config (void)
+{
+ DDRE  = DDRE & 0xEF;  //Set the direction of the PORTE 4 pin as input
+ PORTE = PORTE | 0x10; //Enable internal pull-up for PORTE 4 pin
+}
+
+//Function to configure INT5 (PORTE 5) pin as input for the right position encoder
+void right_encoder_pin_config (void)
+{
+ DDRE  = DDRE & 0xDF;  //Set the direction of the PORTE 4 pin as input
+ PORTE = PORTE | 0x20; //Enable internal pull-up for PORTE 4 pin
+}
+
+void lcd_port_config (void)
+{
+	DDRC = DDRC | 0xF7; //all the LCD pin's direction set as output
+	PORTC = PORTC & 0x80; // all the LCD pins are set to logic 0 except PORTC 7
+}
+void adc_init()
+{
+	ADCSRA = 0x00;
+	ADCSRB = 0x00;		//MUX5 = 0
+	ADMUX = 0x20;		//Vref=5V external --- ADLAR=1 --- MUX4:0 = 0000
+	ACSR = 0x80;
+	ADCSRA = 0x86;		//ADEN=1 --- ADIE=1 --- ADPS2:0 = 1 1 0
+}
+//ADC pin configuration
+void adc_pin_config (void)
+{
+	DDRF = 0x00;
+	PORTF = 0x00;
+	DDRK = 0x00;
+	PORTK = 0x00;
+}
+//Function to initialize ports
+void port_init()
+{
+ motion_pin_config(); //robot motion pins config
+ left_encoder_pin_config(); //left encoder pin config
+ right_encoder_pin_config(); //right encoder pin config	
+ lcd_port_config();
+ adc_pin_config();
+}
+
+void left_position_encoder_interrupt_init (void) //Interrupt 4 enable
+{
+ cli(); //Clears the global interrupt
+ EICRB = EICRB | 0x02; // INT4 is set to trigger with falling edge
+ EIMSK = EIMSK | 0x10; // Enable Interrupt INT4 for left position encoder
+ sei();   // Enables the global interrupt 
+}
+
+void right_position_encoder_interrupt_init (void) //Interrupt 5 enable
+{
+ cli(); //Clears the global interrupt
+ EICRB = EICRB | 0x08; // INT5 is set to trigger with falling edge
+ EIMSK = EIMSK | 0x20; // Enable Interrupt INT5 for right position encoder
+ sei();   // Enables the global interrupt 
+}
+
+//ISR for right position encoder
+ISR(INT5_vect)  
+{
+ ShaftCountRight++;  //increment right shaft position count
+}
+
+
+//ISR for left position encoder
+ISR(INT4_vect)
+{
+ ShaftCountLeft++;  //increment left shaft position count
+}
+
+
+//Function used for setting motor's direction
+void motion_set (unsigned char Direction)
+{
+ unsigned char PortARestore = 0;
+
+ Direction &= 0x0F; 		// removing upper nibbel for the protection
+ PortARestore = PORTA; 		// reading the PORTA original status
+ PortARestore &= 0xF0; 		// making lower direction nibbel to 0
+ PortARestore |= Direction; // adding lower nibbel for forward command and restoring the PORTA status
+ PORTA = PortARestore; 		// executing the command
+}
+
+void forward (void) //both wheels forward
+{
+  motion_set(0x06);
+}
+
+void back (void) //both wheels backward
+{
+  motion_set(0x09);
+}
+
+void left (void) //Left wheel backward, Right wheel forward
+{
+  motion_set(0x05);
+}
+
+void right (void) //Left wheel forward, Right wheel backward
+{
+  motion_set(0x0A);
+}
+
+void soft_left (void) //Left wheel stationary, Right wheel forward
+{
+ motion_set(0x04);
+}
+
+void soft_right (void) //Left wheel forward, Right wheel is stationary
+{
+ motion_set(0x02);
+}
+
+void soft_left_2 (void) //Left wheel backward, right wheel stationary
+{
+ motion_set(0x01);
+}
+
+void soft_right_2 (void) //Left wheel stationary, Right wheel backward
+{
+ motion_set(0x08);
+}
+
+void stop (void)
+{
+  motion_set(0x00);
+}
+
+
+//Function used for turning robot by specified degrees
+void angle_rotate(unsigned int Degrees)
+{
+ float ReqdShaftCount = 0;
+ unsigned long int ReqdShaftCountInt = 0;
+
+ ReqdShaftCount = (float) Degrees/ 4.090; // division by resolution to get shaft count
+ ReqdShaftCountInt = (unsigned int) ReqdShaftCount;
+ ShaftCountRight = 0; 
+ ShaftCountLeft = 0; 
+
+ while (1)
+ {
+  if((ShaftCountRight >= ReqdShaftCountInt) | (ShaftCountLeft >= ReqdShaftCountInt))
+  break;
+ }
+ stop(); //Stop robot
+}
+
+//Function used for moving robot forward by specified distance
+
+void linear_distance_mm(unsigned int DistanceInMM)
+{
+ float ReqdShaftCount = 0;
+ unsigned long int ReqdShaftCountInt = 0;
+
+ ReqdShaftCount = DistanceInMM / 5.338; // division by resolution to get shaft count
+ ReqdShaftCountInt = (unsigned long int) ReqdShaftCount;
+  
+ ShaftCountRight = 0;
+ ShaftCountLeft = 0; 
+ while(1)
+ {
+  if((ShaftCountRight > ReqdShaftCountInt) |  (ShaftCountLeft > ReqdShaftCountInt))
+  {
+  	break;
+  }
+ } 
+ stop(); //Stop robot
+}
+
+void forward_mm(unsigned int DistanceInMM)
+{
+ forward();
+ linear_distance_mm(DistanceInMM);
+}
+
+void back_mm(unsigned int DistanceInMM)
+{
+ back();
+ linear_distance_mm(DistanceInMM);
+}
+
+void left_degrees(unsigned int Degrees) 
+{
+// 88 pulses for 360 degrees rotation 4.090 degrees per count
+ left(); //Turn left
+ angle_rotate(Degrees);
+}
+
+
+
+void right_degrees(unsigned int Degrees)
+{
+// 88 pulses for 360 degrees rotation 4.090 degrees per count
+ right(); //Turn right
+ angle_rotate(Degrees);
+}
+
+
+void soft_left_degrees(unsigned int Degrees)
+{
+ // 176 pulses for 360 degrees rotation 2.045 degrees per count
+ soft_left(); //Turn soft left
+ Degrees=Degrees*2;
+ angle_rotate(Degrees);
+}
+
+void soft_right_degrees(unsigned int Degrees)
+{
+ // 176 pulses for 360 degrees rotation 2.045 degrees per count
+ soft_right();  //Turn soft right
+ Degrees=Degrees*2;
+ angle_rotate(Degrees);
+}
+
+void soft_left_2_degrees(unsigned int Degrees)
+{
+ // 176 pulses for 360 degrees rotation 2.045 degrees per count
+ soft_left_2(); //Turn reverse soft left
+ Degrees=Degrees*2;
+ angle_rotate(Degrees);
+}
+
+void soft_right_2_degrees(unsigned int Degrees)
+{
+ // 176 pulses for 360 degrees rotation 2.045 degrees per count
+ soft_right_2();  //Turn reverse soft right
+ Degrees=Degrees*2;
+ angle_rotate(Degrees);
+}
+
+
+
+
+//velocity control using pwm
+// Timer 5 initialized in PWM mode for velocity control
+// Prescale:256
+// PWM 8bit fast, TOP=0x00FF
+void timer5_init()
+{
+	TCCR5B = 0x00;	//Stop
+	TCNT5H = 0xFF;	//Counter higher 8-bit value to which OCR5xH value is compared with
+	TCNT5L = 0x01;	//Counter lower 8-bit value to which OCR5xH value is compared with
+	OCR5AH = 0x00;	//Output compare register high value for Left Motor
+	OCR5AL = 0xFF;	//Output compare register low value for Left Motor
+	OCR5BH = 0x00;	//Output compare register high value for Right Motor
+	OCR5BL = 0xFF;	//Output compare register low value for Right Motor
+	OCR5CH = 0x00;	//Output compare register high value for Motor C1
+	OCR5CL = 0xFF;	//Output compare register low value for Motor C1
+	TCCR5A = 0xA9;	/*{COM5A1=1, COM5A0=0; COM5B1=1, COM5B0=0; COM5C1=1 COM5C0=0}
+ 					  For Overriding normal port functionality to OCRnA outputs.
+				  	  {WGM51=0, WGM50=1} Along With WGM52 in TCCR5B for Selecting FAST PWM 8-bit Mode*/
+	
+	TCCR5B = 0x0B;	//WGM12=1; CS12=0, CS11=1, CS10=1 (Prescaler=64)
+}
+
+
+
+//Function For ADC Conversion
+unsigned char ADC_Conversion(unsigned char Ch) 
+{
+	unsigned char a;
+	if(Ch>7)
+	{
+		ADCSRB = 0x08;
+	}
+	Ch = Ch & 0x07;  			
+	ADMUX= 0x20| Ch;	   		
+	ADCSRA = ADCSRA | 0x40;		//Set start conversion bit
+	while((ADCSRA&0x10)==0);	//Wait for conversion to complete
+	a=ADCH;
+	ADCSRA = ADCSRA|0x10; //clear ADIF (ADC Interrupt Flag) by writing 1 to it
+	ADCSRB = 0x00;
+	return a;
+}
+
+//Function To Print Sesor Values At Desired Row And Coloumn Location on LCD
+void print_sensor(char row, char coloumn,unsigned char channel)
+{
+	
+	ADC_Value = ADC_Conversion(channel);
+	//lcd_print(row, coloumn, ADC_Value, 3);
+}
+
+//Function for velocity control
+void velocity (unsigned char left_motor, unsigned char right_motor)
+{
+	OCR5AL = (unsigned char)left_motor;
+	OCR5BL = (unsigned char)right_motor;
+}
+
+//Function to initialize all the devices
+void init_devices()
+{
+ cli(); //Clears the global interrupt
+ port_init();  //Initializes all the ports
+ timer5_init();
+ left_position_encoder_interrupt_init();
+ right_position_encoder_interrupt_init();
+ sei();   // Enables the global interrupt 
+}
+
+void do_work(unsigned int x){
+
+	init_devices();
+	float ReqdShaftCount = 0;
+	unsigned long int ReqdShaftCountInt = 0;
+
+	ReqdShaftCount = x / 5.338; /// division by resolution to get shaft count
+	ReqdShaftCountInt = (unsigned long int) ReqdShaftCount;
+	
+	ShaftCountRight = 0;
+	ShaftCountLeft = 0;
+	while(1)
+	{
+
+		Left_white_line = ADC_Conversion(3);	//Getting data of Left WL Sensor
+		Center_white_line = ADC_Conversion(2);	//Getting data of Center WL Sensor
+		Right_white_line = ADC_Conversion(1);	//Getting data of Right WL Sensor
+
+		flag=0;
+
+		//print_sensor(1,1,3);	//Prints value of White Line Sensor1
+		//print_sensor(1,5,2);	//Prints Value of White Line Sensor2
+		//print_sensor(1,9,1);	//Prints Value of White Line Sensor3
+		if((ShaftCountRight > ReqdShaftCountInt) |  (ShaftCountLeft > ReqdShaftCountInt))
+		{
+			stop();
+		}
+		
+		if(Center_white_line>THRESHOLD)
+		{
+			flag=1;
+			forward();
+			velocity(150,150);
+		}
+
+		if((Left_white_line<THRESHOLD) && (flag==0))
+		{
+			flag=1;
+			forward();
+			velocity(130,50);
+		}
+
+		if((Right_white_line<THRESHOLD) && (flag==0))
+		{
+			flag=1;
+			forward();
+			velocity(50,130);
+		}
+
+		if(Center_white_line>THRESHOLD && Left_white_line>THRESHOLD && Right_white_line>THRESHOLD)
+		{
+			forward();
+			velocity(0,0);
+		}
+
+	}
+}
+void returnto_start(unsigned int x)
+{
+	if(x<=4){
+		x=(5-x)*20;
+		forward();
+		do_work(x);
+		_delay_ms(1000);
+		left_degrees(90);
+		_delay_ms(1000);
+		forward();
+		do_work(80);
+	}
+	else
+	{
+		x=5-(x-4);
+		forward();
+		do_work(x*20);
+		_delay_ms(1000);
+		left_degrees(90);
+		_delay_ms(1000);
+		forward();
+		do_work(110);
+	}
+	
+}
+
+void goto_table(unsigned int x)
+{
+	temp=x;
+	if(x<=4)
+	{
+		forward();
+		do_work(30);	
+	}
+	else
+	{
+		forward();
+		do_work(70);
+		x=x-4;
+	}
+	_delay_ms(1000);
+	left_degrees(90);
+	_delay_ms(1000);
+	x=x*20;
+	forward();
+	do_work(x);
+	//drop item
+	
+	returnto_start(temp);
+	
+}
+
+
+//Main Function
+
+int main(void)
+{
+	init_devices();
+
+		forward_mm(200);
+		stop();
+		_delay_ms(1000);
+		left_degrees(90);
+		stop();
+		_delay_ms(1000);
+		forward_mm(200);
+		_delay_ms(1000);
+		right_degrees(90);
+		stop();
+		_delay_ms(1000);
+		forward_mm(200);
+		stop();
+		right_degrees(90);
+		stop();
+		_delay_ms(1000);
+		forward_mm(200);
+		stop();
+		right_degrees(90);
+		stop();
+		_delay_ms(1000);
+		forward_mm(200);
+		stop();
+		right_degrees(90);
+		stop();
+		_delay_ms(1000);
+		forward_mm(200);
+		stop();
+		left_degrees(90);
+		stop();
+		_delay_ms(1000);
+		forward_mm(200);
+		stop();
+		left_degrees(90);
+		stop();
+		_delay_ms(1000);
+		forward_mm(200);
+		_delay_ms(1000);
+		left_degrees(90);
+		_delay_ms(1000);
+		stop();
+		
+		
+		
+}
